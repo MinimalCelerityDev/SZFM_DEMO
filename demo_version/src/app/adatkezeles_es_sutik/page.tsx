894e22cfc0883382721ebb499ccf2bb9bbff341d
MinimalCelerity <info@minimalcelerity.com>
2024-11-23 15:01:58 +0100

    Adatkezelés és süti oldal hozzáadása

---
+++ demo_version/src/app/adatkezeles_es_sutik/page.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const AdatkezelesEsSutik: React.FC = () => {
+  return (
+    <div className="bg-white text-black min-h-screen flex flex-col items-center justify-center">
+      <h1 className="text-3xl font-bold mb-6">Adatkezelés és Sütik</h1>
+      <p className="text-lg text-center max-w-2xl">
+        Itt találod az adatvédelmi és sütikezelési szabályzatunkat. Kérjük, olvasd el figyelmesen!
+      </p>
+    </div>
+  );
+};
+
+export default AdatkezelesEsSutik;
